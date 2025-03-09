a21a64b6923b52564f1c4b5b186cc5b7228284a9
Yudi <12040397+Yudi@users.noreply.github.com>
2025-03-09 14:30:38 -0300

    Initial commit

---
+++ src/app/components/main/main.component.ts
@@ -0,0 +1,24 @@
+import { Component } from "@angular/core";
+import { FormsModule, ReactiveFormsModule } from "@angular/forms";
+import { MatButtonModule } from "@angular/material/button";
+import { MatFormFieldModule } from "@angular/material/form-field";
+import { MatSelectModule } from "@angular/material/select";
+import { MatIconModule } from "@angular/material/icon";
+import { MatInputModule } from "@angular/material/input";
+
+@Component({
+  selector: "app-main",
+  standalone: true,
+  imports: [
+    MatButtonModule,
+    MatInputModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MatFormFieldModule,
+    MatSelectModule,
+    MatIconModule,
+  ],
+  templateUrl: "./main.component.html",
+  styleUrl: "./main.component.css",
+})
+export class MainComponent {}
